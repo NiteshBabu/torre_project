b0de9a51c17174354d0d47b13fb4686cc042c549
NiteshBabu <niteshbabusharma@gmail.com>
2021-02-04 19:36:00 +0530

    included header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="text-center text-warning">
+      <h1>Torre Genome</h1>
+    </div>
+  )
+}
+
+export default Header
