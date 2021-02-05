36b363fb323b796f60bab24002d8abd43d1c94b7
NiteshBabu <niteshbabusharma@gmail.com>
2021-02-05 14:14:52 +0530

    included build files for production

---
+++ build/precache-manifest.a5ebb25960206958822fd56969d1740f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0a88e0140503d5fb85f2eec1caa286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa52840a9d2e1474971",
+    "url": "/static/css/main.2061003a.chunk.css"
+  },
+  {
+    "revision": "72c39f052c9c9724431f",
+    "url": "/static/js/2.2414c017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2414c017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa52840a9d2e1474971",
+    "url": "/static/js/main.aa3db8f6.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
